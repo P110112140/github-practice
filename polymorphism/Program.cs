ad47b2c1bc2716b378b1e9894484f32fcd013bc7
P110112140 <P110112140@student.peterborough.ac.uk>
2020-10-22 10:09:56 +0100

    new update

---
+++ polymorphism/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int addedsumnum = addtwonumbers(7,13);
+            Console.WriteLine("int " + addedsumnum);
+            double addedsumdouble = addtwonumbers(7,13);
+            Console.WriteLine("double " + addedsumdouble);
+            //int addedsumint = addtwoints(7,13);
+            //double addedsumdouble = addtwodoubles(7.6,13.8);
+            //Console.WriteLine("int " + addedsumint);
+            //Console.WriteLine("double " + addedsumdouble);
+        }
+
+        static int addtwonumbers(int x, int y)
+        {
+            return x +y;
+        }
+
+        static double addtwonumbers(double x, double y)
+        {
+            return x +y;
+        }
+        //
+        //static int addtwoints(int x, int y)
+        //{
+        //    return x +y;
+        //}
+
+        //static double addtwodoubles(double x, double y)
+        //{
+        //    return x +y;
+        //}
+        //
+    }
+}
